43fa91a3fe6a76c36ca0e64343f15c2415bb7c5c
nonetypes <nonetypes@gmail.com>
2023-01-02 22:28:01 -0800

    Comment reflects project status

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 # Simplex Calculator
 # Alexander Ralston
 # Created 11//2022
-# Last updated 01/01/2023
+# Last updated 01/02/2023
 #
 # Simplex Method Calculator for educational purposes.
 # George Dantzig's simplex linear programming algorithm for solving optimization problems.
 #
-# TODO: Simplex calculations
-#       Display steps on solve screen
+# TODO: Display steps on solve screen
 #       Redo buttons with icons
 #       Redo settings screen -- add text size input
 #       README.md with screenshots and examples
